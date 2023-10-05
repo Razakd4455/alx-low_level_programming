5fca989fe9a982a614cc1eb3e17df15c0e619683
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-05 16:13:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 	#include <stdio.h>
+	/*The main function does not return any value*/
 	int main(void){
-		/*The function puts prints with a line*/
-	puts("Programming is like building a mulitilingual puzzle");
+	/*The function puts prints with a line*/
+	puts(/"Programming is like building a mulitilingual puzzle");
+	}
+	{
+	/*the return function ends the program*/	
 	return 0;
 	}
