30861b049f74d34fb07db872e30a12178af11def
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-05 17:33:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-/** 
- * Main - Entry point
- * Return : Always 0 (success)
- */
 int main(void)
 /**
- * This is the entry point
- * takes no value
- */
+ * Main - Entry point
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+/**
+ * Return : Always 0 (success)
+*/
 }
