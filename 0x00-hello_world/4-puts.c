3282c05d3cbfcc5c4ea24079cbdec522744e692b
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-05 17:48:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/**
+ * Main - Prints Programming is like building a multilingual
+ *  puzzle, followed by a new line.*/
 int main(void)
-/* Main - Entry point*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
