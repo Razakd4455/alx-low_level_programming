60f6136096dfa728a257dffeb1126819c2bde357
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-06 19:34:11 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+return (0);
+}
