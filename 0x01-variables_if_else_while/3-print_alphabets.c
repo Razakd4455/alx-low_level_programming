4d448f4aa953a21c81c94ecf129f671f17f17441
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-06 20:11:05 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - main function
  *
- *Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,6 +11,7 @@ for (i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
 }
+putchar('\n');
 for (i = 'A'; i <= 'Z'; i++)
 {
 putchar(i);
