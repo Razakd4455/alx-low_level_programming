a13e0e82823675d6c30da45f612433c4d3d4a82e
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-10 10:04:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,10 @@ void print_alphabet_x10(void)
 	int i;
 for (i = 0; i < 10; i++)
 }
-char alphabet = 'a';
-while (alphabet <= 'z')
+char alphabet
+for (alphabet = 'a'; <= 'z'; alphabet++)
 {
 	_putchar(alphabet);
-	alphabet++;
 }
 _putchar('\n');
 }
