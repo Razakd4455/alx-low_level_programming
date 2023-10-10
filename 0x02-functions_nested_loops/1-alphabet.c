c9eb58c276053752811fcbd164905eff3b85cb0c
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-10 08:27:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - checks the code
  *
  * return: always 0
  */
-int main(void)
+void print_alphabet(void);
 {
 char alphabet = 'a';
 while (alphabet <= 'z')
