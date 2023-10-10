60976909ec4c0df6cfbf24eaa37871bc1b51bf4c
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-10 04:18:01 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+char main(void)
+{
+putchar("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A readme file for nested loops directory in the second week of learning program in C
