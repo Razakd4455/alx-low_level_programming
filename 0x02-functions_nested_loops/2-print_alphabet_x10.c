ffefc48cce7c5e1894bc75145db6121184083fa0
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-10 09:41:58 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+for (int i = 0; i < 10; i++)
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+	_putchar(alphabet);
+	alphabet++;
+}
+_putchar('\n');
+}
