7df960bb78b7ac392cef9431d61f44b68576a2bf
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 10:16:06 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,10 @@ void print_most_numbers(void)
 
 {
 int num;
-	for (num = 0; num <= 9; num++)
-		if (num != 2 && num != 4)
-		{
-		_putchar((num % 10) + '0');
-		}
+for (num = 0; num <= 9; num++)
+{
+if (num != 2 && num != 4)
+_putchar((num % 10) + '0');
+}
 _putchar('\n');
-return (0);
 }
