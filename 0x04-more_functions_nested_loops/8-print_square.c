67b66133b98aa47257c559f2e9830cc56ae9a4b0
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 15:10:53 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - print square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+
+{
+int x, y;
+if (size <= 0)
+}
+_putchar('\n');
+{
+else
+{
+for (x = 0; x <= 0; x++)
+{
+for (y = 0; y < size; y++)
+}
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
