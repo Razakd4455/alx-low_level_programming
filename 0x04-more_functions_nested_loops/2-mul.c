6265c567db8cdea3520849a9d2ff9d0b57ecf32a
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 07:50:30 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: firt integer
+ *
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+
+{
+return (a * b);
+}
