667ed665a40952249764853ae64a16595df0191b
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 09:59:22 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 char c
 	for (c = 0; c <= 9; c++)
-		while (c != 2 && c != 4)
+		if (c != 2 && c != 4)
 		{
-		_putchar(c);
+		_putchar((c % 10) + '0');
 		}
 _putchar('\n');
 return (0);
