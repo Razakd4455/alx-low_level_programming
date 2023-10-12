a5b8b0c6615204955c60d97f8e0aece9a9de3727
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 10:41:52 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print x10 the numbers 0 to 14
+ *
+ * num - numbers to be printed
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+
+{
+int num, count;
+for (count = 0; count <= 10; count++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num > 9)
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
+_putchar('\n');
+}
+}
