9435e837fdc0379350fe14f5d66f303fd7e8d189
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 16:36:08 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * fizz_buzz - prints numbers,Fizz, Buzz and FizzBuzz
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int x = 0;
+
+	while (x <= 100)
+	{
+	if (x % 3 == 0 && x % 5 == 0)
+	printf("FizzBuzz");
+	else if (x % 5 == 0)
+	{
+	if (x == 100)
+	{
+	printf("Buzz");
+	printf('\n');
+	}
+	else
+	printf("Buzz");
+	}
+	else if (x % 3 == 0)
+	printf("Fizz");
+	else
+	printf("%d ", x);
+	x++;
+	}
+	return (0);
+}
+
