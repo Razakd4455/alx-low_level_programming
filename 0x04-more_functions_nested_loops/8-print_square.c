5e5b115b085b05ec11e5a275830340c40063342b
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-12 15:18:00 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,15 @@ void print_square(int size)
 {
 int x, y;
 if (size <= 0)
-}
-_putchar('\n');
 {
+_putchar('\n');
+}
 else
 {
 for (x = 0; x <= 0; x++)
 {
 for (y = 0; y < size; y++)
-}
+{
 _putchar(35);
 }
 _putchar('\n');
