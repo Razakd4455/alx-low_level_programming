598327a31402c7a857229608bfbddf784fbc3517
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-13 03:11:50 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 
 {
 int num, count;
-for (count = 0; count <= 10; count++)
+for (count = '0'; count <= '10'; count++)
 {
-for (num = 0; num <= 14; num++)
+for (num = '0'; num <= '14'; num++)
 {
-if (num > 9)
+if (num > '9')
 {
 _putchar((num / 10) + '0');
 }
