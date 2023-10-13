849e9adfcaf701a75ea92bc7dc9b74ab7dc149c7
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-13 04:08:43 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,25 @@
 int main(void)
 
 {
-	int x = 1;
-
-	while (x <= 100)
-	{
-	if (x % 3 == 0 && x % 5 == 0)
-	printf("FizzBuzz");
-	else if (x % 5 == 0)
-	{
-	if (x == 100)
-	{
-	printf("Buzz");
-	printf('\n');
-	}
-	else
-	printf("Buzz");
-	}
-	else if (x % 3 == 0)
-	printf("Fizz");
-	else
-	printf("%d ", x);
-	x++;
-	}
-	return (0);
+for (int x = 1; x <= 100; x++)
+{
+if (x % 3 == 0 && x % 5 == 0)
 }
-
+printf("FizzBuzz");
+{
+else if (x % 3 == 0)
+}
+printf("Fizz");
+{
+else if (x % 5 == 0)
+}
+printf("Buzz");
+{
+else
+}
+printf("%d", x);
+{
+printf('\n'); 
+return (0);
+}
+}	
