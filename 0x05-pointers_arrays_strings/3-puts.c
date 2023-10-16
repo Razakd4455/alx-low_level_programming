902bb3c57a493aea9e6432d8dd71a9e6b5a05bb8
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-16 13:56:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_puts - prints a string
+ *
+ *@str: the sring to print
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+
+_putchar('\n');
+
+}
