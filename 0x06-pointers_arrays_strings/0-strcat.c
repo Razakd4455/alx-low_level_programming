b29e76a10974cf6807beecd0598455dd4746a398
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-19 14:07:29 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ *
+ * @dest: string to concatenate to
+ *
+ *@src: char string
+ *
+ *Return: pointer to resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+for (a = 0; dest[a] != '\0'; a++)
+{
+for (b = 0; src[b] != '\0'; b++)
+dest[a] = src[b];
+a++
+}
+dest[a] = '\0';
+return (dest);
+}
