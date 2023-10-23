802e80ca862138a369e3aba5355546f256f5b540
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-23 12:09:02 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - searches for the occureance of the character c in the string s*
+ *@s:sring
+ *@c:character to be searched for
+ *Return:return a pointer to the first occurrence of the character c in
+ *the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (NULL);
+}
