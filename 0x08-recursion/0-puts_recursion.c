461974e9f07026daf1a7819a715025e588c151df
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-25 12:12:16 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_puts_recursion - recursive function to print to screen
+ *
+ * @s: string
+ *
+ *Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+
+if (*s != '\0')
+{
+_putchar(*s);
+_puts(s + 1);
+}
+if (*s == '\0')
+{
+_putchar('\n');
+}
+}
