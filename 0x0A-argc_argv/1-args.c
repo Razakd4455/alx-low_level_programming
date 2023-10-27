f6c05a1cc73e1c142b251babc7ee97db87f0e64a
Razak Dimmie <dimmierazak000@outlook.com>
2023-10-27 10:26:56 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc:number of command line arg..
+ * @argv:array that contain command line arg..
+ * Return: (0) when sucessful
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+return (0);
+}
