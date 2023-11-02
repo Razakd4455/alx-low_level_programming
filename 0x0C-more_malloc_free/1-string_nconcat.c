10ca9218bd56b5081fbd43b608630c79f4b8a19b
Razak Dimmie <dimmierazak000@outlook.com>
2023-11-02 12:53:13 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,43 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
+
 /**
- * string_nconcat - concates two string and first n bytes s2
- * @s1:string 1
- * @s2:string 2
- * @n:number of n bytes
- * Return:returns pointer or NULL
+ * string_nconcat - concatenates two strings and the first n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes from s2 to concatenate
+ * Return: returns a pointer to the concatenated string or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-	char *ptr;
-	unsigned int i, j;
-	unsigned int length1 = 0;
-	unsigned int length2 = 0;
-
-	if (s1 == NULL)
-	s1 = "";
-	if (s2 == NULL)
-	s2 = "";
-	while (s1[length1])
-	length1++;
-	while (s2[length2])
-	length2++;
-	if (n >= length2)
-	n = length2;
-	ptr = malloc(sizeof(char) * (length1 + length2 + 1));
-	if (ptr == NULL)
-	return (NULL);
-	for (i = 0; i < length1; i++)
-	ptr[i] = s1[i];
-	for (j = 0; j < n; j++)
+char *ptr;
+size_t i, j, k;
+if (s1 == NULL)
+i = 0;
+else
+{
+for (i = 0; s1[i] != '\0'; i++)
 	{
-	ptr[i] = s2[j];
-	i++;
 	}
-	return (ptr);
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+}
+if (j > n)
+j = n;
+ptr = malloc(sizeof(char) * (i + j + 1));
+if (ptr == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+ptr[k] = s1[k];
+for (k = 0; k < j; k++)
+ptr[k + i] = s2[k];
+ptr[i + j] = '\0';
+return (ptr);
 }
