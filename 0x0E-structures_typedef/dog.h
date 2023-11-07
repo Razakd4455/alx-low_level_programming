27efc5c224e8908592a6cb99b2e09f4ca32679fc
Razak Dimmie <dimmierazak000@outlook.com>
2023-11-07 01:47:40 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - a structure to represent information about a dog
+ * @name: dog name
+ * @age:age of dog
+ * @owner:name of dog owner
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
