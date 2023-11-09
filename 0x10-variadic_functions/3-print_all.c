bb1def8f21f32639107a27978845c135f8227498
Razak Dimmie <dimmierazak000@outlook.com>
2023-11-09 14:43:53 -0800

    updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -38,8 +38,7 @@ break;
 default:
 break;
 }
-if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
-			format[i] == 'f' || format[i] == 's'))
+if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 printf(", ");
 i++;
 }
